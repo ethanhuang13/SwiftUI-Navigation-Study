b38b94b3817011fe2b6e0b528ab3b28ef980f36d
Ethan Huang <eh13@hey.com>
2021-10-03 16:39:30 +0800

    Initial Commit

---
+++ Shared/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Shared
+//
+//  Created by Ethan Huang on 2021/10/3.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Shared/NavApp.swift
@@ -0,0 +1,17 @@
+//
+//  NavApp.swift
+//  Shared
+//
+//  Created by Ethan Huang on 2021/10/3.
+//
+
+import SwiftUI
+
+@main
+struct NavApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
